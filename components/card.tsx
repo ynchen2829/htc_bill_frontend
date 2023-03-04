71932c9785b4c0b8d563a4d68cec05cb74a9cd9a
ynchen2829 <ynchen2829@gmail.com>
2023-03-04 08:11:29 -0600

    save changes again

--- components/card.tsx
+++ components/card.tsx
@@ -1,9 +1,22 @@
+import { useRouter } from "next/router";
+
 export default function Card(bill:any){
-    console.log(bill)
     let content = bill["bill"]
+    console.log("in card ",bill);
+    const router = useRouter();
+    const handleClick = async (e:any) => {
+        e.preventDefault();
+        router.push({
+            pathname: "/home/"+content["name"],
+            query:{
+              content
+            }
+          });
+    }
+
     return(
-        <div className="max-w-full p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            <a href="#">
+        <div onClick={handleClick} className="max-w-full p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <a>
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{content["name"] + " " +content["title"]}</h5>
             </a>
             <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{content["description"]}</p>
