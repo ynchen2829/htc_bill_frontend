bc5468b0c8cd3a0eae0a3fabcda1f7d36adbac63
Ruofan Liu <ruofanliu7@gmail.com>
2023-03-04 11:30:18 -0600

    card final (hopefully)

--- components/card.tsx
+++ components/card.tsx
@@ -16,7 +16,19 @@ export default function Card(bill:any){
 
     return(
         <div onClick={handleClick} className="flex overflow-hidden flex-direction-row items-center justify-start max-w-full p-0 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">     
-            <div className="relative block pt-10 px-20 pb-2.5 ml-[-100px] mr-[-75px] -rotate-90 bg-blue-200">Complete</div>
+            {
+                (() => {
+                    if (content["status"] == 2)
+                        return <div className="relative block pt-10 px-20 pb-2.5 ml-[-100px] mr-[-75px] -rotate-90 bg-green-200">COMPLETE</div>
+                    if (content["status"] == 1 || content["stage"] == 0)
+                        return <div className="relative block pt-10 px-20 pb-2.5 ml-[-100px] mr-[-75px] -rotate-90 bg-yellow-200">ACTIVE</div>
+                    else (content["status"] == 0)
+                        return <div className="relative block pt-10 px-20 pb-2.5 ml-[-100px] mr-[-75px] -rotate-90 bg-red-200">DEAD</div>
+                })()
+            }
+            
+            
+            {/* <div className="relative block pt-10 px-20 pb-2.5 ml-[-100px] mr-[-75px] -rotate-90 bg-blue-200">Complete</div> */}
             <a className="block w-full pr-0 m-5">
                 <h6 className = "mb-2 text-l tracking-tight text-gray-900 dark:text-white">{content["name"]}</h6>
                 <h5 className="inline-block w-5/6 mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{content["title"]}</h5>
